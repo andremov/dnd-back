c9186dd527b7b3b735f3caa529fa1a5d5323e0d2
Andres-XL <amovilla1103@gmail.com>
2020-12-27 01:30:13 -0500

    missing a couple of fields

--- api/models/players.js
+++ api/models/players.js
@@ -1,9 +1,26 @@
 const mongoose = require('mongoose');
 
-const productSchema = mongoose.Schema({
+const playerSchema = mongoose.Schema({
     _id : mongoose.Schema.Types.ObjectId,
-    name : {type: String, required: true},
-    price : {type: Number, required: true}
+    name : { type : String, required : true },
+    family : { type : String, required : true },
+    codename : { type : String, required : true, unique : true },
+    
+    alignment : { type : Number, required : true },
+    char_class : { type : Number, required : true },
+    race : { type : Number, required : true },
+    height : { type : Number, required : true },
+    age : { type : Number, required : true },
+    
+    level : { type : Number, required : true, default : 1 },
+    hit_points : { type : Number, required : true },
+    max_hit_points : { type : Number, required : true },
+    
+    inventory : { type : String },
+    notes : { type : String },
+    armor_class : { type : Number, default : 0 },
+    attack_dice_sides : { type : Number, default : 2 },
+    attack_dice_number : { type : Number, default : 1 }
 });
 
-module.exports = mongoose.model('Product', productSchema);
+module.exports = mongoose.model('player', playerSchema);
--- api/routes/players.js
+++ api/routes/players.js
@@ -1,91 +1,88 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-// const Product = require('../models/product');
+const Player = require('../models/players');
 
-// ALL REQUESTS AND PATHS
 router.get('/', async ( req, res ) => {
-    res.status(200).json({ response : 'Hello.' })
-    // Product.find()
-    //     .select('name price _id')
-    //     .exec()
-    //     .then(doc => {
-    //         console.log(doc);
-    //         res.status(200).json({
-    //             count : doc.length,
-    //             products : doc.map(item =>{
-    //                 return {
-    //                     name : item.name,
-    //                     price : item.price,
-    //                     _id : item._id
-    //                 }
-    //             })
-    //         });
-    //     })
-    //     .catch(err=>{
-    //         console.log(err);
-    //         res.status(500).json({error:err});
-    //     })
+    Player.find()
+        .exec()
+        .then(doc => {
+            res.status(200).json(doc);
+        })
+        .catch(err => {
+            res.status(500).json({ error : err });
+        })
 });
 
 router.post('/', async ( req, res ) => {
-    // const product = new Product({
-    //     _id: new mongoose.Types.ObjectId(),
-    //     name : req.body.name,
-    //     price : req.body.price
-    // });
-    // product.save().then( doc => {
-    //     res.status(200).json(doc);
-    // }).catch(err =>{
-    //     res.status(500).json({
-    //         error: err
-    //     });
-    // })
+    const player = new Player({
+        ...req.body,
+        _id : new mongoose.Types.ObjectId(),
+    });
+    player.save()
+        .then(() => {
+            res.status(200).json({ message : 'Success!' });
+        }).catch(err => {
+        res.status(500).json({
+            error : err
+        });
+    })
+});
+
+router.get('/find', async ( req, res ) => {
+    let codename = req.query.codename;
+    Player.findOne({ codename }).exec().then(doc => {
+        if ( doc ) {
+            res.status(200).json(doc);
+        } else {
+            res.status(404).json({
+                message : 'Product not found.'
+            });
+        }
+    }).catch(err => {
+        res.status(500).json({
+            error : err
+        });
+    })
 });
 
 router.get('/:id', async ( req, res ) => {
-    // let id = req.params.id;
-    // Product.findById(id).exec().then(doc => {
-    //     if (doc) {
-    //         res.status(200).json(doc);
-    //     } else {
-    //         res.status(404).json({
-    //             message: 'Product not found.'
-    //         });
-    //     }
-    // }).catch( err => {
-    //     res.status(500).json({
-    //         error: err
-    //     });
-    // })
+    let id = req.params.id;
+    Player.findById(id).exec().then(doc => {
+        if ( doc ) {
+            res.status(200).json(doc);
+        } else {
+            res.status(404).json({
+                message : 'Product not found.'
+            });
+        }
+    }).catch(err => {
+        res.status(500).json({
+            error : err
+        });
+    })
 });
 
 router.patch('/:id', async ( req, res ) => {
-    // let id = req.params.id;
-    // const updateOps = {};
-    // for (const ops of req.body) {
-    //     updateOps[ops.propName] = ops.value;
-    // }
-    // Product.update({_id:id},{$set: updateOps}).exec().then(doc => {
-    //     res.status(200).json(doc);
-    // }).catch(err => {
-    //     res.status(500).json({
-    //         error: err
-    //     });
-    // })
+    let id = req.params.id;
+    Player.update({ _id : id }, { ...req.body }).exec().then(doc => {
+        res.status(200).json(doc);
+    }).catch(err => {
+        res.status(500).json({
+            error : err
+        });
+    })
 });
 
 router.delete('/:id', async ( req, res ) => {
-    // let id = req.params.id;
-    // Product.deleteOne({_id : id}).exec().then (doc => {
-    //     res.status(200).json(doc);
-    // }).catch(err => {
-    //     res.status(500).json({
-    //         message: err
-    //     });
-    // });
+    let id = req.params.id;
+    Player.deleteOne({ _id : id }).exec().then(doc => {
+        res.status(200).json(doc);
+    }).catch(err => {
+        res.status(500).json({
+            message : err
+        });
+    });
 });
 
-
-// EXPORT PATH
 module.exports = router;
