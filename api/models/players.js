c1a0874cc1f0560d9334c132a323f2bf2cf3f886
Andres-XL <amovilla1103@gmail.com>
2021-01-02 20:04:01 -0500

    family is not required

--- api/models/players.js
+++ api/models/players.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const playerSchema = mongoose.Schema({
     _id : mongoose.Schema.Types.ObjectId,
     name : { type : String, required : true },
-    family : { type : String, required : true },
+    family : { type : String },
     codename : { type : String, required : true, unique : true },
     
     alignment : { type : Number, required : true },
