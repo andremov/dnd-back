1fc071cbac754d2c2161ac510c8e3f44a1188d0f
Andres-XL <amovilla1103@gmail.com>
2021-01-05 22:21:54 -0500

    adding quest objectives

--- api/models/quests.js
+++ api/models/quests.js
@@ -4,7 +4,8 @@ const questSchema = mongoose.Schema({
     _id : mongoose.Schema.Types.ObjectId,
     owner : { type : mongoose.Schema.Types.ObjectId, required : true, ref : 'player' },
     name : { type : String, required : true },
-    data : { type : String }
+    data : { type : String },
+    objectives : { type : String },
 });
 
 module.exports = mongoose.model('quest', questSchema);
